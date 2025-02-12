094c56f09ecc00b90453e0b89dd221bdc78ab168
bohdana_kuzmenko <bogdana.kuzmenko.16@gmail.com>
2025-02-12 21:27:51 +0100

    Refactor logging in Terraform action handlers

--- src/terry/presentation/cli/screens/main/mixins/terraform_action_handler_mixin.py
+++ src/terry/presentation/cli/screens/main/mixins/terraform_action_handler_mixin.py
@@ -1,6 +1,5 @@
 import asyncio
 
-
 from terry.domain.terraform.core.entities import TerraformFormatScope, CommandStatus
 from terry.infrastructure.shared.command_process_context_manager import CommandProcessContextManager
 from terry.infrastructure.shared.command_utils import process_stdout_stderr
@@ -54,23 +53,18 @@ class TerraformActionHandlerMixin:
         """
         format_scope = None
         if event.format.value == TerraformFormatScope.CURRENT_FILE.value:
-            content_tabs = self.query_one(Content)
+            content_tabs = self.query_one(Content)  # type: ignore
             format_scope = content_tabs.active_tab
             if not format_scope:
-                self.notify("No file selected.", severity="warning")
+                self.notify("No file selected.", severity="warning")  # type: ignore
                 return
-
         try:
-            output = self.terraform_core_service.fmt(format_scope)
-        except TerraformFormatException as e:
-            self.log.error(str(e))
-            self.notify("Failed to apply format settings.", severity="error")
-            self.write_command_log(e.command, CommandStatus.ERROR, e.message)
+            output = self.terraform_core_service.fmt(format_scope)  # type: ignore
+        except TerraformFormatException as ex:
+            self._log_error("Failed to apply format settings.", ex.command, ex.message)
             return
         else:
-            self.notify("Format settings applied successfully.", severity="information")
-            self.log.info(output.output)
-            self.write_command_log(output.command, CommandStatus.SUCCESS, output.output)
+            self._log_success("Format settings applied successfully.", output.command, output.output)
 
     def on_validate_action_request(self, event: ValidateActionRequest) -> None:
         """
@@ -85,16 +79,11 @@ class TerraformActionHandlerMixin:
                 details, such as the scope of formatting to be applied.
         """
         try:
-            output = self.terraform_core_service.validate(event.settings)
-        except TerraformValidateException as e:
-            self.log.error(str(e))
-            self.notify("Project validation failed.", severity="error")
-            self.write_command_log(e.command, CommandStatus.ERROR, e.message)
+            output = self.terraform_core_service.validate(event.settings)  # type: ignore
+        except TerraformValidateException as ex:
+            self._log_error("Project validation failed.", ex.command, ex.message)
             return
-        else:
-            self.notify("Project has been validate.", severity="information")
-            self.log.info(output.output)
-            self.write_command_log(output.command, CommandStatus.SUCCESS, output.output)
+        self._log_success("Project has been validated.", output.command, output.output)
 
     async def on_plan_action_request(self, event: PlanActionRequest):
         """
@@ -104,18 +93,18 @@ class TerraformActionHandlerMixin:
         Arguments:
             event (PlanApplyRequest): The event containing the plan settings to apply.
         """
-        self.push_screen(TerraformCommandOutputScreen())
+        self.push_screen(TerraformCommandOutputScreen())  # type: ignore
         command = TerraformPlanCommandBuilder().build_from_settings(event.settings)
         if self._tf_command_executor:
             self._tf_command_executor.cancel()
 
-        worker = self.run_worker(
+        worker = self.run_worker(  # type: ignore
             self.run_tf_action(command, "Failed to apply plan settings."),
             exit_on_error=True,
             thread=True,
             group="tf_command_worker",
         )
-        self._tf_command_executor = TerraformCommandExecutor(command=command, worker=worker)
+        self._tf_command_executor = TerraformCommandExecutor(command=command, worker=worker)  # type: ignore
 
     async def on_init_action_request(self, event: InitActionRequest):
         """
@@ -130,18 +119,19 @@ class TerraformActionHandlerMixin:
             event (InitActionRequest): The initialization apply request
                 event that triggers this handler.
         """
-        self.push_screen(TerraformCommandOutputScreen())
+        self.push_screen(TerraformCommandOutputScreen())  # type: ignore
         command = TerraformInitCommandBuilder().build_from_settings(event.settings)
 
         if self._tf_command_executor:
             self._tf_command_executor.cancel()
 
-        worker = self.run_worker(
+        worker = self.run_worker(  # type: ignore
             self.run_tf_action(command, error_message="Failed to apply plan settings."),
             exit_on_error=True,
             thread=True,
             group="tf_command_worker",
         )
+
         self._tf_command_executor = TerraformCommandExecutor(command=command, worker=worker)
 
     async def on_apply_action_request(self, event: ApplyActionRequest):
@@ -158,14 +148,13 @@ class TerraformActionHandlerMixin:
                 applying the Terraform plan.
 
         """
-        # Todo: check if we can reuse logic for init, plan, apply
-        self.push_screen(TerraformCommandOutputScreen())
+        self.push_screen(TerraformCommandOutputScreen())  # type: ignore
         command = TerraformApplyCommandBuilder().build_from_settings(event.settings)
 
         if self._tf_command_executor:
             self._tf_command_executor.cancel()
 
-        worker = self.run_worker(
+        worker = self.run_worker(  # type: ignore
             self.run_tf_action(command, error_message="Failed to apply settings."),
             exit_on_error=True,
             thread=True,
@@ -190,9 +179,10 @@ class TerraformActionHandlerMixin:
         """
 
         await asyncio.sleep(2)
+
         tf_command_str = " ".join(tf_command)
-        area = self.app.query_one(TerraformCommandOutputScreen)
-        manager = CommandProcessContextManager(tf_command, str(self.work_dir))
+        area = self.app.query_one(TerraformCommandOutputScreen)  # type: ignore
+        manager = CommandProcessContextManager(tf_command, str(self.work_dir))  # type: ignore
         self._tf_command_executor.command_process = manager  # type: ignore
         with manager as (stdin, stdout, stderr):
             area.stdin = stdin
@@ -201,7 +191,35 @@ class TerraformActionHandlerMixin:
         area.stdin = None
 
         if manager.error:
-            self.log.error(str(manager.error))
-            self.notify(error_message, severity="error")
-            self.write_command_log(tf_command_str, CommandStatus.ERROR, str(manager.error))
+            self._log_error(error_message, tf_command_str, str(manager.error))
             return
+
+    def _log_success(self, message: str, command, details: str):
+        """
+        Log the success of a Terraform command.
+
+        This method logs the success of a Terraform command by writing the output and command to the log.
+
+        Args:
+            message (str): Command main message.
+            command (str): The Terraform command that was executed
+            details (str): The output of the Terraform command that was executed
+        """
+        self.notify(message, severity="information")  # type: ignore
+        self.log.info(details)  # type: ignore
+        self.write_command_log(command, CommandStatus.SUCCESS, details)  # type: ignore
+
+    def _log_error(self, message: str, command: str, error_message: str):
+        """
+        Log the error of a Terraform command.
+
+        This method logs the error of a Terraform command by writing the error and command to the log.
+
+        Args:
+            message (str): Command main message.
+            command (str): The Terraform command that was executed
+            error_message (str): The error message of the Terraform command that was executed
+        """
+        self.log.error(error_message)  # type: ignore
+        self.notify(message, severity="error")  # type: ignore
+        self.write_command_log(command, CommandStatus.ERROR, error_message)  # type: ignore
